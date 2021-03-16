1891af94e08411887dcbd90108c67d5c2eac914b
Stas Kukarkin <staskykarkin@gmail.com>
2021-03-16 16:22:24 +0600

    third

--- script.js
+++ script.js
@@ -165,6 +165,7 @@ window.addEventListener('DOMContentLoaded', function(){
             }
         });
 
+        
         for (let i = 0; i < input.length; i++){
             input[i].value='';
         }
